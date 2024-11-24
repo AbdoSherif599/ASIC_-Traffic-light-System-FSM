4f604d9d4ee20af5a948a48b85adb93416adcf06
Abdelrahman Sherif <abdelrahmansherif599@gmail.com>
2024-11-24 16:18:20 +0200

    add counter feature

--- traffic.v
+++ traffic.v
@@ -4,14 +4,72 @@ output reg [4:1] traffic;
 input [8:1] sensors;
 
 parameter T1 = 4'b0001, T2 = 4'b0010, T3 = 4'b0100, T4 = 4'b1000;
-
+reg [5:0] counter;
 reg [3:0] current_state, next_state;
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
         current_state <= T1;
+        counter<=0;
     end else begin
-        current_state <= next_state;
+        if (counter==0) begin
+
+            current_state <= next_state;
+             case(current_state)
+                T1: begin
+                    if (sensors[1]&sensors[2]) begin
+                        counter=60;
+                    end
+                    else if(sensors[1]|sensors[2])begin
+                      counter =30;
+                    end
+                    else begin
+                      counter=0;
+                    end
+                end
+                T2: begin
+                    if (sensors[3]&sensors[4]) begin
+                        counter=60;
+                    end
+                    else if(sensors[3]|sensors[4])begin
+                      counter =30;
+                    end
+                    else begin
+                      counter=0;
+                    end
+
+                end
+                T3: begin
+                    if (sensors[5]&sensors[6]) begin
+                        counter=60;
+                    end
+                    else if(sensors[5]|sensors[6])begin
+                      counter =30;
+                    end
+                    else begin
+                      counter=0;
+                    end
+
+                end
+                T4: begin
+                    if (sensors[7]&sensors[8]) begin
+                        counter=60;
+                    end
+                    else if(sensors[7]|sensors[8])begin
+                      counter =30;
+                    end
+                    else begin
+                      counter=0;
+                    end
+
+                end
+                default: counter = 0;
+            endcase            
+        end
+        else begin
+          counter<=counter-1;
+        end
+        
     end
 end
 
