8beecd6f2e9f566d292ad57bf6b84c586f6102a8
Abdelrahman Sherif <abdelrahmansherif599@gmail.com>
2024-11-26 21:08:12 +0200

    edits on test_bench

--- tb.v
+++ tb.v
@@ -4,13 +4,15 @@ module tb;
 
 reg clk, rst;
 reg [8:1] sensors;
+integer file_id;
 
 wire [4:1] result;
+reg  [7:0] mm [9:0];
 
 initial begin
     $dumpfile("test.fst");
     $dumpvars(0, tb);
-
+    file_id=$fopen("test_bench_test_values.txt","w");
     clk = 0;
     rst = 0;
 
@@ -25,6 +27,7 @@ initial begin
     //If all congested loop in order T1 to T4 waits 10s 
     sensors='b11111111;
     #500
+    $fwrite(file_id,"%h",result);
     //If sensors for T1 are all fired then it waits 10s or else 5s
     sensors='b01110111; // T1 10 sec  T2 5 sec  T3 10 sec  T4 5 sec
     #500
@@ -37,6 +40,7 @@ initial begin
     #200
     sensors='b01000000; // act after getting any read of the sensors
     #200
+    $fclose(file_id);
     $finish;
 end
 
